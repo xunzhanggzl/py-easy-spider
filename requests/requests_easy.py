4c51c6e4a4a202590b9bdab085ab9765abb3eb51
xunzhanggzl <1243286264@qq.com>
2019-11-25 16:36:00 +0800

    add readme.md

--- requests/requests_easy.py
+++ requests/requests_easy.py
@@ -8,6 +8,7 @@ def getHTMLText(url):
         r.raise_for_status()
         print(r.apparent_encoding)
         print(r.encoding)
+        print(r.headers)
         # https://2.python-requests.org/en/master/api/#requests.Response.apparent_encoding
         r.encoding = r.apparent_encoding
         # https://2.python-requests.org/en/master/api/#requests.Response.text
